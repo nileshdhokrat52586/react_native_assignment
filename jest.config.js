b55e645161698fe0266a2ee63765d73d9788fbfa
nileshdhokrat52586 <60600721+nileshdhokrat52586@users.noreply.github.com>
2025-10-06 19:24:32 +0530

    complete assignment

--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,3 @@
 module.exports = {
   preset: 'react-native',
-  testEnvironment: 'node',
-  transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|react-clone-referenced-element|@react-navigation|@react-native-community|@react-native-picker)/)',
-  ],
-  setupFilesAfterEnv: [
-    '@testing-library/jest-native/extend-expect',
-  ],
-  testPathIgnorePatterns: [
-    '/node_modules/',
-    '/android/',
-    '/ios/',
-  ],
-  moduleFileExtensions: [
-    'ts',
-    'tsx',
-    'js',
-    'jsx',
-    'json',
-    'node',
-  ],
-  collectCoverage: false,
-  verbose: true,
 };
